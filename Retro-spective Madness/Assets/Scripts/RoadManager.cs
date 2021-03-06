75bb258a7294e35fa77d6479cc54cafc94084e58
Kybi99 <59123928+Kybi99@users.noreply.github.com>
2021-03-06 23:36:22 +0100

    Intro Scene & Portal Demo(Day 1)
    Added Cowboy assets, Pistol assets, Desert assets and Western assets. Created run animation, couple of materials and scripts for time manipulation and player controller scripts.

---
+++ Retro-spective Madness/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoadManager : MonoBehaviour
+{
+    public GameObject[] segments;
+    public float speed = .1f;
+    float segmentSize = 10f;
+
+    float startTime = 0f;
+    float holdTime = 5f;
+
+    private void Update()
+    {
+        MoveSegments();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("PortalScene");
+    }
+
+    private void MoveSegments()
+    {
+       
+        if (Input.GetKey(KeyCode.R))
+        {
+            Invoke("ChangeScene", 2.0f);
+            
+
+            for (int i = 0; i < segments.Length; i++)
+            {
+                float z = segments[i].transform.position.z;
+                z += speed;
+                z = Mathf.Round(z * 10) / 10;
+
+                segments[i].transform.position = new Vector3(0, 0, z);
+                if (segments[i].transform.position.z >= segmentSize * (segments.Length - 1))
+                {
+                    segments[i].transform.position = new Vector3(0, 0,  -segmentSize);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < segments.Length; i++)
+            {
+                float z = segments[i].transform.position.z;
+                z -= speed;
+                z = Mathf.Round(z * 10) / 10;
+
+                segments[i].transform.position = new Vector3(0, 0, z);
+                if (segments[i].transform.position.z <= -segmentSize)
+                {
+                    segments[i].transform.position = new Vector3(0, 0, (segments.Length - 1) * segmentSize);
+                }
+            }
+        }
+    }
+}
---
+++ Retro-spective Madness/Assets/Scripts/Travel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Travel : MonoBehaviour
+{
+    public Transform player;
+    public Transform destination;
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = destination.transform.position;
+    }
+
+
+  
+}
